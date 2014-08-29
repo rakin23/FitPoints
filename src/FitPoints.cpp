98a033461345e8a5c1b2fa95f7bed9072b850e45
Andrew Snyder <andrew.snyder.email@gmail.com>
2014-08-28 21:50:02 -0400

    Minor changes.

--- src/FitPoints.cpp
+++ src/FitPoints.cpp
@@ -7,9 +7,30 @@
 //============================================================================
 
 #include <iostream>
+#include "rapidxml.hpp"
+#include <string>
+#include <fstream>
+#include <sstream>
 using namespace std;
+using namespace rapidxml;
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+int main()
+{
+   xml_document<> doc;
+   std::ifstream file("activity_575641024.tcx");
+   std::stringstream buffer;
+   buffer << file.rdbuf();
+   file.close();
+   std::string content(buffer.str());
+   doc.parse<0>(&content[0]);
+   xml_node<> *pRoot = doc.first_node();
+   xml_node<> *pNode = pRoot->first_node();
+   xml_attribute<> *pAttr = pNode->first_attribute("attribute");
+   int nValue = atoi(pAttr->value());
+   cout << nValue;
+
+   // With the xml example above this is the <document/> node
+
+   cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+   return 0;
 }
